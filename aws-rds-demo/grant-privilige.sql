94a34f4a3f4c9cf2cdd129364654f6d0e60267cf
yjyoo6831 <cenabro@naver.com>
2024-08-26 18:53:01 +0900

    aws-test-deploy

--- aws-rds-demo/grant-privilige.sql
+++ aws-rds-demo/grant-privilige.sql
@@ -2,3 +2,7 @@ select user from mysql.user;
 create user 'sesac1'@'%' identified with mysql_native_password by '1234';
 
 select User,Host,plugin from mysql.user;
+
+show tables;
+
+select * from Users;
