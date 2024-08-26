5d6d2b48a6eb9807668b80e831caea32a20058a7
yjyoo6831 <cenabro@naver.com>
2024-08-26 15:35:00 +0900

    aws-rds-demo

---
+++ aws-rds-demo/grant-privilige.sql
@@ -0,0 +1,4 @@
+select user from mysql.user;
+create user 'sesac1'@'%' identified with mysql_native_password by '1234';
+
+select User,Host,plugin from mysql.user;
---
+++ aws-rds-demo/models/User.js
@@ -0,0 +1,24 @@
+const {DataTypes, Sequelize} =  require('sequelize');
+
+module.exports = (sequelize) => {
+    return sequelize.define('User',{
+        id:{
+            type:DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement : true,
+        },
+        username:{
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        email:{
+            type:DataTypes.STRING,
+            allowNull: false,
+            unique : true,
+        }
+    },
+    {
+        freezeTableName: true,
+    }
+)
+}
